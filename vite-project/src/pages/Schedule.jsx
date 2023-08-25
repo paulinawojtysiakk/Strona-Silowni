36b6188b408731ca062e3d3ffdd805389fa3ee3e
paulinawojtysiakk <paulina.wwojtysiak@gmail.com>
2023-08-25 19:09:54 +0200

    updated table wih signing up and cancelling classes + incrementation and decrementation

--- vite-project/src/pages/Schedule.jsx
+++ vite-project/src/pages/Schedule.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TableSchedule from './TableSchedule'
+ import '/src/scss/Schedule.scss'
 
 const Schedule = () => {
     return (
--- vite-project/src/pages/TableSchedule.jsx
+++ vite-project/src/pages/TableSchedule.jsx
@@ -28,25 +28,81 @@ function TableSchedule() {
             };
         }, []);
 
-        async function getGymClasses() {
-            const { data } = await supabase.from("gymClasses").select();
+
+    async function getGymClasses() {
+        const { data, error } = await supabase.from("gymClasses").select();
+        if (error) {
+            console.error("Error getting gym classes", error.message);
+        } else {
             setGymClasses(data);
         }
+    }
+
+    async function signUpForClass(classId) {
+        const targetClass = gymClasses.find((item) => item.id === classId);
+        const updatedParticipants = targetClass.participants + 1; // Inkrementacja uczestników
+
+        const { data: gymClass, error  } = await supabase
+            .from("gymClasses")
+            .update({
+                signed_up: true,
+                participants: updatedParticipants
+
+            })
+            .eq("id", classId)
+            .single();
+
+        if (error) {
+            console.error("Error signing up", error.message);
+        } else {
+            console.log("Signed up successfully");
+            const updatedClasses = gymClasses.map((item) =>
+                item.id === gymClass.id ? gymClass : item
+            );
+            setGymClasses(updatedClasses);
+        }
+    }
+
+    async function cancelSignUpForClass(classId) {
+        const targetClass = gymClasses.find((item) => item.id === classId);
+        const updatedParticipants = Math.max(targetClass.participants - 1, 0);// Dekrementacja
+
+        const { data: gymClass, error } = await supabase
+            .from("gymClasses")
+            .update({
+                signed_up: false,
+                participants: updatedParticipants
+            })
+            .eq("id", classId)
+            .single();
+
+        if (error) {
+            console.error("Error canceling sign-up", error.message);
+        } else {
+            console.log("Sign-up canceled");
+            const updatedClasses = gymClasses.map((item) =>
+                item.id === gymClass.id ? gymClass : item
+            );
+            setGymClasses(updatedClasses);
+        }
+    }
+
 
         return <div>
-            <h2 style={{ textAlign: "center", padding:'10px'}}><strong>Zajęcia Grupowe</strong></h2>
+            <h2 style={{ textAlign: "center", padding:'30px', fontSize: '20px'}}><strong>Zajęcia Grupowe</strong></h2>
             <ul style={{ listStyle: "none"}}>
                 <li
                     style={{
                         display: "flex",
                         justifyContent: "space-between",
                         alignItems: "center",
-                        borderBottom: "1px solid #ccc",
-                        padding: "15px 0",
-                        fontWeight: "bold",
-                    }}
-                >
-                    <div style={{flex:1, padding: '10px' }}>Zajęcia</div>
+                        borderBottom: "3px solid #ccc",
+                        borderTop: "3px solid #ccc",
+                        padding: "40px 0",
+                        textAlign:'center'
+                    }}>
+                    <div style={{flex:1 }}>Zajęcia</div>
+                    <div style={{ flex: 1 }}>Kiedy</div>
                     <div style={{ flex: 1 }}>Czas</div>
                     <div style={{ flex: 1 }}>Dla kogo</div>
                     <div style={{ flex: 1 }}>Cel</div>
@@ -56,27 +112,36 @@ function TableSchedule() {
 
                 </li>
                 {gymClasses.map(gymClass => <li
-                        key={gymClass.name}
+                        key={gymClass.id}
                         style={{
                             display: "flex",
                             justifyContent: "space-between",
                             alignItems: "center",
                             borderBottom: "1px solid #ccc",
-                            padding: "10px 0",
+                            padding: "15px 0",
                         }}>
-                        <div style={{flex: 1 }}>{gymClass.name}</div>
-                        <div style={{ flex: 1 }}>{gymClass.time}</div>
+                        <div style={{flex: 1, fontSize:'16px', fontWeight:'bold' }}>{gymClass.name}</div>
+                        <div style={{ flex: 1, textAlign:'center' }}>{gymClass.when}</div>
+                        <div style={{ flex: 1, textAlign:'center' }}>{gymClass.time} min</div>
                         <div style={{ flex: 1 }}>{gymClass.who}</div>
-                        <div style={{ flex: 1 }}>{gymClass.goal}</div>
-                        <div style={{ flex: 1, textAlign:'center'  }}>{gymClass.participants}</div>
-                        <div style={{ flex: 1, textAlign:'center'  }}>
+                        <div style={{ flex: 1, }}>{gymClass.goal}</div>
+                        <div style={{ flex: 1, textAlign:'center' }}>{gymClass.participants}</div>
+                        <div style={{ flex: 1, textAlign:'center' }}>
                             {Math.max(gymClass.total_places - gymClass.participants, 0)}
                         </div>
-                        <div style={{ flex: 1 }} >{gymClass.signed_up}
-                            <Button variant="outlined" className='btn-cta-home'>Zapisuję się!</Button>
-                        </div>
+                    <div>
+                        {gymClass.signed_up ? (
+                            <div>
+                                <div style={{maxWidth: '130px', paddingBottom: '10px'}}>Twoje miejsce zostało zarezerwowane</div>
+                                <Button variant='outlined' onClick={() => cancelSignUpForClass(gymClass.id)}>Anuluj</Button>
+                            </div>
+                        ) : (
+                        <Button variant='outlined' onClick={() => signUpForClass(gymClass.id)}>Zapisuję się!</Button>
+                    )}
+                    </div>
+                </li>
+                   )}
 
-                    </li>)}
             </ul>
         </div>;
     }
